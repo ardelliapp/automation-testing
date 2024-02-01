217b2770bd9881da5c45594c6e9cfdb49d9e5a4f
Ardellia Putri Pertiwi <62167031+ardelliapp@users.noreply.github.com>
2024-02-01 15:37:16 +0700

    changed

---
+++ Object Repository/Page_Login - KG Media ID/div_KG Media ID atau password yang kamu masukkan salah.rs
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_KG Media ID atau password yang kamu masukkan salah</name>
+   <tag></tag>
+   <elementGuidId>e0bccacc-1629-4d4f-8fd1-99906b3cc5ee</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form/div[3]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.form-error.text-center</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>fd7a3485-c4b8-434b-b181-8a47103f9d57</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-error text-center</value>
+      <webElementGuid>f8d8f224-ee3f-466c-9c4b-87816d04a280</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            KG Media ID atau password yang kamu masukkan salah
+        </value>
+      <webElementGuid>96a22f1d-0680-4d64-a612-08e6e3cac8db</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;container&quot;]/div[@class=&quot;block-middle&quot;]/div[@class=&quot;form-main&quot;]/form[1]/div[@class=&quot;form-error text-center&quot;]</value>
+      <webElementGuid>8a759b73-be6a-4792-b889-bb4981ad9e57</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div[3]</value>
+      <webElementGuid>01ee170a-41eb-48e8-b6e1-3bc7b27a3384</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+            KG Media ID atau password yang kamu masukkan salah
+        ' or . = '
+            KG Media ID atau password yang kamu masukkan salah
+        ')]</value>
+      <webElementGuid>ae87c8f7-c04c-4828-8b47-2ef75a8e873c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
